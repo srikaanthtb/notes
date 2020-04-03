fbaac972ee15adab87cef2fd12ddc6e07843613d
srikaanthtb <srikaanth.t.b@gmail.com>
2020-04-03 14:45:23 +0100

    step 8 complete

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -8,9 +8,10 @@ Notelistview.prototype.showlist = function () {
 string = "<ul>"
  for (let i = 0; i < this.notelist.givelist().length; i++) {
    var n = this.notelist.givelist()[i].text
-   console.log(n);
-   var singlenoteview = new Singlenoteview(n);
-   string += "<li>" + singlenoteview.shownote() + "</li>"
+   console.log(n.slice(0,20));
+  // var singlenoteview = new Singlenoteview(n);
+  var showsinglenote = n.slice(0,20);
+   string += "<li><div>" + showsinglenote + "</div></li>"
  }
  string = string + "</ul>"
  return string
--- tests/note-controller-test.js
+++ tests/note-controller-test.js
@@ -1,7 +1,7 @@
 let notelistdouble = new Double({
   noteadd: function() {},
   givelist: function() {
-    return [{text: "yo"}, {text: "son"}]
+    return [{text: "yoasdasdasdasdasdasdasdasdasd"}, {text: "son"}]
   }
 });
 var noteController = new NoteController(notelistdouble);
@@ -13,8 +13,9 @@ CheckInstanceOfNoteController();
 
 
 function CheckHTMLisReturning() {
-var expectstring = "<ul><li><div>yo</div></li><li><div>son</div></li></ul>";
+var expectstring = "<ul><li><div>yoasdasdasdasdasdasd</div></li><li><div>son</div></li></ul>";
 console.log(expectstring);
+console.log(noteController.insertHTML());
   assert.isTrue(expectstring === noteController.insertHTML())
 };
 CheckHTMLisReturning();
